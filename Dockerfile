a09ae55e9bb8ffdc1bb2824489bb39bfb52d9f25
kozakura913 <98575220+kozakura913@users.noreply.github.com>
2024-04-07 22:53:15 +0900

    wip:リモートからのファイルをそのまま返すだけ

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+FROM rust:alpine
+RUN apk add --no-cache musl-dev curl
+RUN curl -sSL https://github.com/mozilla/sccache/releases/download/v0.7.7/sccache-v0.7.7-x86_64-unknown-linux-musl.tar.gz | tar -zxf - -C /tmp && mv /tmp/sccache*/sccache /usr/local/bin && rm -rf /tmp/sccache*
+ENV CARGO_HOME=/var/cache/cargo
+RUN mkdir /app
+WORKDIR /app
+COPY src ./src
+COPY Cargo.toml ./Cargo.toml
+COPY Cargo.lock ./Cargo.lock
+RUN --mount=type=cache,target=/var/cache/cargo cargo fetch --locked
+ENV RUSTC_WRAPPER=/usr/local/bin/sccache
+ENV SCCACHE_DIR=/var/cache/sccache
+RUN --mount=type=cache,target=/var/cache/cargo --mount=type=cache,target=/var/cache/sccache cargo build --target x86_64-unknown-linux-musl --release --offline
+
+FROM alpine:latest
+ARG UID="852"
+ARG GID="852"
+RUN addgroup -g "${GID}" proxy && adduser -u "${UID}" -G proxy -D -h /media-proxy-rs -s /bin/sh proxy
+WORKDIR /media-proxy-rs
+USER proxy
+COPY --from=0 /app/target/x86_64-unknown-linux-musl/release/media-proxy-rs ./media-proxy-rs
+EXPOSE 12766
+CMD ["./media-proxy-rs"]
---
+++ src/main.rs
@@ -0,0 +1,81 @@
+use std::{io::Write, net::SocketAddr, sync::Arc};
+
+use axum::{response::IntoResponse, Router};
+use serde::{Deserialize, Serialize};
+
+fn main() {
+	let config_path=match std::env::var("FILES_PROXY_CONFIG_PATH"){
+		Ok(path)=>{
+			if path.is_empty(){
+				"config.json".to_owned()
+			}else{
+				path
+			}
+		},
+		Err(_)=>"config.json".to_owned()
+	};
+	if !std::path::Path::new(&config_path).exists(){
+		let default_config=ConfigFile{
+			bind_addr: "0.0.0.0:12766".to_owned(),
+			timeout:1000,
+			user_agent: "https://github.com/yojo-art/media-proxy-rs".to_owned(),
+		};
+		let default_config=serde_json::to_string_pretty(&default_config).unwrap();
+		std::fs::File::create(&config_path).expect("create default config.json").write_all(default_config.as_bytes()).unwrap();
+	}
+	let config:ConfigFile=serde_json::from_reader(std::fs::File::open(&config_path).unwrap()).unwrap();
+
+	let config=Arc::new(config);
+	let rt=tokio::runtime::Builder::new_multi_thread().enable_all().build().unwrap();
+	rt.block_on(async{
+		let http_addr:SocketAddr = config.bind_addr.parse().unwrap();
+		let client=reqwest::Client::new();
+		let app = Router::new();
+		let client0=client.clone();
+		let config0=config.clone();
+		let app=app.route("/",axum::routing::get(move|path,headers,parms|get_file(path,headers,client0.clone(),parms,config0.clone())));
+		let app=app.route("/*path",axum::routing::get(move|path,headers,parms|get_file(path,headers,client.clone(),parms,config.clone())));
+		axum::Server::bind(&http_addr).serve(app.into_make_service_with_connect_info::<SocketAddr>()).await.unwrap();
+	});
+}
+
+async fn get_file(
+	axum::extract::Path(_path):axum::extract::Path<String>,
+	headers:axum::http::HeaderMap,
+	client:reqwest::Client,
+	axum::extract::Query(q):axum::extract::Query<RequestParams>,
+	config:Arc<ConfigFile>,
+)->axum::response::Response{
+	let mut headers=axum::headers::HeaderMap::new();
+	headers.append("X-Remote-Url",q.url.parse().unwrap());
+	let req=client.get(q.url);
+	let req=req.timeout(std::time::Duration::from_millis(config.timeout));
+	let req=req.header("UserAgent",config.user_agent.clone());
+	let resp=match req.send().await{
+		Ok(resp) => resp,
+		Err(e) => return (axum::http::StatusCode::BAD_REQUEST,headers,format!("{:?}",e)).into_response(),
+	};
+	fn add_remote_header(key:&'static str,headers:&mut axum::headers::HeaderMap,remote_headers:&reqwest::header::HeaderMap){
+		for v in remote_headers.get_all(key){
+			headers.append(key,String::from_utf8_lossy(v.as_bytes()).parse().unwrap());
+		}
+	}
+	let remote_headers=resp.headers();
+	add_remote_header("Content-Disposition",&mut headers,remote_headers);
+	add_remote_header("Content-Type",&mut headers,remote_headers);
+	let response_bytes=match resp.bytes().await{
+		Ok(resp)=>resp,
+		Err(e)=>return (axum::http::StatusCode::BAD_GATEWAY,headers,format!("{:?}",e)).into_response(),
+	};
+	(axum::http::StatusCode::OK,headers,response_bytes).into_response()
+}
+#[derive(Debug,Serialize,Deserialize)]
+pub struct ConfigFile{
+	bind_addr: String,
+	timeout:u64,
+	user_agent:String,
+}
+#[derive(Debug, Deserialize)]
+pub struct RequestParams{
+	url: String,
+}
