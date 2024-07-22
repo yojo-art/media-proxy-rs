e050c35096e3a0e0042056bf033e519c31970310
kozakura913 <98575220+kozakura913@users.noreply.github.com>
2024-07-23 00:39:36 +0900

    moldリンカを使う

--- crossfiles/build.sh
+++ crossfiles/build.sh
@@ -5,6 +5,9 @@ else
 	source /app/crossfiles/${TARGETARCH}/${TARGETVARIANT}.sh
 fi
 cp -r /dav1d/lib /${MUSL_NAME}/dav1d/lib
+mkdir ./.cargo/
+echo "[target.${RUST_TARGET}]" >> ./.cargo/config.toml
+echo 'rustflags = ["-C", "link-arg=-fuse-ld=/usr/bin/mold"]' >> ./.cargo/config.toml
 cargo build --release --target ${RUST_TARGET}
 cargo build --release --target ${RUST_TARGET} --example healthcheck
 cp /app/target/${RUST_TARGET}/release/media-proxy-rs /app/media-proxy-rs
